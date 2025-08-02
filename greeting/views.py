6b9152b52b1946f0080edfd8f79dfe88143a2618
Agonzayesu <aagonzayesu@student.refactory.academy>
2025-08-02 12:13:55 +0300

    installed black

--- greeting/views.py
+++ greeting/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 
 def index(request):
-    return render(request, 'index.html', {'greeting': 'Hello'})
+    return render(request, "index.html", {"greeting": "Hello"})
